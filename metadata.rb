8a1a80c8d811cb53150490f3a32625d0c4da10a0
Tim Smith <tsmith@chef.io>
2017-03-20 11:55:27 -0700

    Move templates out of the default directory
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'gems'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Sets up a local gem server repository or mirror'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '2.0.0'
